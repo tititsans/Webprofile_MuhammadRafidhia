825a92ee0dce7285699e97108c0a775e7fe9e0d0
tititsans <raffidhia@gmail.com>
2023-10-07 16:35:18 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// // Contoh 1: if, else, nested if
+// var nilai = 75;
+
+// if (nilai >= 80) {
+//     console.log("Nilai Anda A");
+// } else if (nilai >= 70) {
+//     console.log("Nilai Anda B");
+// } else {
+//     console.log("Nilai Anda C");
+// }
+
+// // Contoh 2: switch case
+// var hari = "Senin";
+
+// switch (hari) {
+//     case "Senin":
+//     console.log("Hari ini awal pekan.");
+//     break;
+//     case "Selasa":
+//     case "Rabu":
+//     case "Kamis":
+//     console.log("Hari kerja di pertengahan minggu.");
+//     break;
+//     case "Jumat":
+//     console.log("Hari terakhir sebelum akhir pekan.");
+//     case "Sabtu":
+//     case "Minggu":
+//     console.log("Hari ini akhir pekan.");
+//     break; 
+//     default:
+//     console.log("Hari libur atau nama hari tidak valid.");
+// }
+
+// // Contoh 3: for statement
+// console.log("Menggunakan for statement:");
+
+// for (var i = 1; i <= 5; i++) {
+//     console.log("Iterasi ke-" + i);
+// }
+
+// // Contoh 4: while statement
+// console.log("Menggunakan while statement:");
+// var counter = 1;
+
+// while (counter <= 5) {
+//     console.log("Iterasi ke-" + counter);
+//     counter++;
+// }
+
+// Contoh 5: do-while statement
+console.log("Menggunakan do-while statement:");
+var hitung = 1;
+
+do {
+    console.log("Iterasi ke-" + hitung);
+    hitung++;
+} while (hitung <= 5);
+
+// // Contoh 6: function
+// function tambah(a, b) {
+//     return a + b;
+// }
+
+// var hasil = tambah(3, 5);
+// console.log("Hasil penambahan: " + hasil);
+
